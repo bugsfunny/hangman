55ce19337d5a35e8bfab63fafec2d58a1b502cb0
bugsfunny <js.denize@gmail.com>
2022-10-09 09:15:04 +0200

    Failed commit

--- hangman/hangman.go
+++ hangman/hangman.go
@@ -1,6 +1,9 @@
 package hangman
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 const ALREADY_GUESSED = "alreadyGuessed"
 const GOOD_GUESS = "goodGuess"
@@ -16,7 +19,10 @@ type Game struct {
 	TurnsLeft    int
 }
 
-func New(turns int, word string) *Game {
+func New(turns int, word string) (*Game, error) {
+	if len(word) < 3 {
+		return nil, fmt.Errorf("word '%s' must be at least 3 characters, got=%v", word, len(word))
+	}
 	letters := strings.Split(strings.ToUpper(word), "")
 	foundLetters := make([]string, len(word))
 	for i := range letters {
@@ -29,26 +35,31 @@ func New(turns int, word string) *Game {
 		UsedLetters:  []string{},
 		TurnsLeft:    turns,
 	}
-	return g
+	return g, nil
 }
 
 func (g *Game) MakeAGuess(guess string) {
+	switch g.State {
+	case WON, LOST:
+		return
+	}
 	guess = strings.ToUpper(guess)
-	if letterInWorld(guess, g.UsedLetters) {
+	if letterInWord(guess, g.UsedLetters) {
 		g.State = ALREADY_GUESSED
-	} else if letterInWorld(guess, g.Letters) {
+	} else if letterInWord(guess, g.Letters) {
 		g.State = GOOD_GUESS
 		g.revealLetter(guess)
 		if hasWon(g.Letters, g.FoundLetters) {
 			g.State = WON
 		}
 	} else {
+		g.State = BAD_GUESS
+
 		g.UsedLetters = append(g.UsedLetters, guess)
 		g.TurnsLeft--
-		if g.TurnsLeft > 0 {
-			g.State = BAD_GUESS
-		} else {
-			g.State = LOST
+
+		if g.TurnsLeft <= 0 {
+			g.State = "lost"
 		}
 
 	}
@@ -72,7 +83,7 @@ func hasWon(letters []string, foundLetters []string) bool {
 	return true
 }
 
-func letterInWorld(guess string, letters []string) bool {
+func letterInWord(guess string, letters []string) bool {
 	for _, v := range letters {
 		if guess == v {
 			return true
